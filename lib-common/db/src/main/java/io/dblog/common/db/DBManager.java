fa6d2342ed44251eb4035050bd4900e3286b676e
lipenglong <1589987691@qq.com>
2017-07-18 22:52:00 +0800

    lib-common: init DB

---
+++ lib-common/db/src/main/java/io/dblog/common/db/DBManager.java
@@ -0,0 +1,13 @@
+package io.dblog.common.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Pelin on 17/7/18.
+ */
+public class DBManager {
+
+}
