e4c6f2deff0e56f3f6577294879f5f482773f011
lipenglong <1589987691@qq.com>
2017-08-03 22:02:37 +0800

    lib-proto: add some proto files

--- sso/src/main/java/io/dblog/sso/service/sign/LoginService.java
+++ sso/src/main/java/io/dblog/sso/service/sign/LoginService.java
@@ -1,7 +1,25 @@
 package io.dblog.sso.service.sign;
 
+import io.dblog.sso.controller.sign.LoginForm;
+import io.dblog.sso.controller.sign.RegisterForm;
+import io.dblog.sso.service.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Pelin on 17/8/1.
  */
+@Service
 public class LoginService {
+
+
+    @Autowired
+    private AccountService accountService;
+
+    public void login(HttpServletRequest request, LoginForm form) {
+
+    }
+
 }
--- sso/src/main/java/io/dblog/sso/service/sign/RegisterService.java
+++ sso/src/main/java/io/dblog/sso/service/sign/RegisterService.java
@@ -1,7 +1,20 @@
 package io.dblog.sso.service.sign;
 
+import io.dblog.sso.controller.sign.RegisterForm;
+import io.dblog.sso.service.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by Pelin on 17/8/1.
  */
 public class RegisterService {
+
+    @Autowired
+    private AccountService accountService;
+
+    public void register(HttpServletRequest request, RegisterForm form) {
+
+    }
 }
